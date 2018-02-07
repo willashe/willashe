1a54d0d1a693675ff8dfd4f8893f62c783600d32
Will Ashe <willashe@hotmail.com>
2018-02-06 19:47:18 -0600

    contact form styling

--- src/scripts/components/ContactForm.js
+++ src/scripts/components/ContactForm.js
@@ -10,28 +10,18 @@ class ContactForm extends PureComponent {
   render() {
     return (
       <div className="contact-form" aria-label="Contact Form">
-        Contact form!
+        <h1>Contact</h1>
         <form>
-          <ul>
-            <li>
-              <label htmlFor="name-input">Name: </label>
-              <input
-                ref={input => {
-                  this.nameInput = input;
-                }}
-                id="name-input"
-                type="text"
-              />
-            </li>
-            <li>
-              <label htmlFor="email-input">Email: </label>
-              <input id="email-input" type="text" />
-            </li>
-            <li>
-              <label htmlFor="message-input">Message: </label>
-              <textarea id="message-input" />
-            </li>
-          </ul>
+          <input
+            placeholder="Name"
+            ref={input => {
+              this.nameInput = input;
+            }}
+            id="name-input"
+            type="text"
+          />
+          <input placeholder="Email" id="email-input" type="text" />
+          <textarea placeholder="Message" id="message-input" />
         </form>
       </div>
     );
